ace9246634fcdb28b308e88b9094889de408a50b
jjflash0777-creator <jjflash0777@gmail.com>
2026-09-08 15:38:53 -0400

    feat: load Phase 4 homepage style layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./styles/design-system.css";
+import "./styles/homepage-v4.css";
 import { AnalyticsBridge } from "./components/AnalyticsBridge";
 import { PublicShell } from "./components/PublicShell";
 
